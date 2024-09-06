6d695591f3c39151284f34d4cc95cdddc361eb99
Darren <nardol.darren@gmail.com>
2024-09-06 16:30:41 +0200

    install webpack encore + bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\SweatShirts;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ManagerRegistry $manager): Response
     {
+        $products = $manager->getRepository(SweatShirts::class)->findAll();
+        $users = $manager->getRepository(User::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'users' => $users,
+            'products' => $products
         ]);
     }
 }
