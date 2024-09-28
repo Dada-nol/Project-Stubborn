0e12f55895d68f1364171153b4099e95cafb07ab
Darren <nardol.darren@gmail.com>
2024-09-28 13:32:38 +0200

    add Sweat and Size fixtures

---
+++ src/DataFixtures/SweatFixtures.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\SweatShirts;
+use App\Service\FileUploader;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class SweatFixtures extends Fixture
+{
+
+  public function load(ObjectManager $manager): void
+  {
+
+    $sweatshirtData = [
+      [
+        'name' => 'Blackbelt',
+        'price' => 29.90,
+        'isPromoted' => false,
+        'ImageFilename' => 'Blackbelt.jpeg',
+      ],
+
+      [
+        'name' => 'BlueBelt',
+        'price' => 29.90,
+        'isPromoted' => false,
+        'ImageFilename' => 'BlueBelt.jpeg',
+      ],
+
+      [
+        'name' => 'Street',
+        'price' => 34.50,
+        'isPromoted' => false,
+        'ImageFilename' => 'Street.jpeg',
+      ],
+
+      [
+        'name' => 'Pokeball',
+        'price' => 45.00,
+        'isPromoted' => true,
+        'ImageFilename' => 'Pokeball.jpeg',
+      ],
+
+      [
+        'name' => 'PinkLady',
+        'price' => 29.90,
+        'isPromoted' => false,
+        'ImageFilename' => 'PinkLady.jpeg',
+      ],
+
+      [
+        'name' => 'Snow',
+        'price' => 32.00,
+        'isPromoted' => false,
+        'ImageFilename' => 'Snow.jpeg',
+      ],
+
+      [
+        'name' => 'Greyback',
+        'price' => 28.50,
+        'isPromoted' => false,
+        'ImageFilename' => 'Greyback.jpeg',
+      ],
+
+      [
+        'name' => 'BlueCloud',
+        'price' => 45.00,
+        'isPromoted' => false,
+        'ImageFilename' => 'BlueCloud.jpeg',
+      ],
+
+      [
+        'name' => 'BornInUsa',
+        'price' => 59.90,
+        'isPromoted' => true,
+        'ImageFilename' => 'BornInUsa.jpeg',
+      ],
+
+      [
+        'name' => 'GreenSchool',
+        'price' => 42.20,
+        'isPromoted' => true,
+        'ImageFilename' => 'GreenSchool.jpeg',
+      ],
+
+    ];
+
+    foreach ($sweatshirtData as $data) {
+      $sweatshirt = new SweatShirts();
+      $sweatshirt->setName($data['name']);
+      $sweatshirt->setPrice($data['price']);
+      $sweatshirt->setisPromoted($data['isPromoted']);
+      $sweatshirt->setImageFilename($data['ImageFilename']);
+
+      $manager->persist($sweatshirt);
+    }
+    $manager->flush();
+  }
+}
