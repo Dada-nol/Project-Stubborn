cc55a7719fcafcbd1014475f425f5589d0573571
Darren <nardol.darren@gmail.com>
2024-10-15 12:19:51 +0200

    setupt for railway

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Installer les dépendances PHP
+composer install --no-dev --optimize-autoloader --classmap-authoritative
+
+composer require doctrine/doctrine-fixtures-bundle 
+
+# Exécuter les migrations de la base de données
+# php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+
+# Charger les fixtures (si nécessaire)
+# php bin/console doctrine:fixtures:load --no-interaction
+
+# Lancer Apache (ou tout autre service principal)
+exec apache2-foreground
